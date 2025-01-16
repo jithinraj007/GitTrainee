895bb737681170c5765d4130966e5072f6ffd0e3
Jithinraj KJ <87666270+jithinraj007@users.noreply.github.com>
2025-01-16 13:55:58 +0530

    InitiAL COMMIT

--- Models/ErrorViewModel.cs
+++ Models/ErrorViewModel.cs
@@ -3,6 +3,7 @@ namespace GitTrainee.Models
     public class ErrorViewModel
     {
         public string? RequestId { get; set; }
+        public string? UserName { get; set; }
 
         public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
     }
