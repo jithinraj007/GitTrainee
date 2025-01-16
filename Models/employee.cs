83bf62e503ce435e81ad8ac978f6aceab02a05a0
Aswin <aswinashokanuk95@gmail.com>
2025-01-16 14:03:04 +0530

    add emplyee

---
+++ Models/employee.cs
@@ -0,0 +1,11 @@
+﻿namespace GitTrainee.Models
+{
+	public class employee
+	{
+		public int Id { get; set; }
+		public string Name { get; set; }
+		public string Description { get; set; }
+		public int EmployeeId { get; set; }
+
+	}
+}
